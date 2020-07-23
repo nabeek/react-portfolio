92b80a60be94bf87d69020ae699aebc42d8054de
nabeek <nabeek@gmail.com>
2020-07-22 21:41:22 -0600

    Updated dev dependencies

---
+++ build/precache-manifest.58b14077f669f761d9522a0493d1a839.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e43f4eda810effa96131ca4992533139",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "e898aedb25737d8f7139",
+    "url": "/react-portfolio/static/css/main.16ddee95.chunk.css"
+  },
+  {
+    "revision": "aa497b71d6a89abddf4c",
+    "url": "/react-portfolio/static/js/2.08867fc8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.08867fc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e898aedb25737d8f7139",
+    "url": "/react-portfolio/static/js/main.3077fd27.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
