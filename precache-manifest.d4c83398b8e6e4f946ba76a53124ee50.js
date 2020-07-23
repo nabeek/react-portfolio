444f34aca641d243a2143471ead24f310c32ea31
nabeek <nabeek@gmail.com>
2020-07-22 22:06:12 -0600

    Updates

--- precache-manifest.d4c83398b8e6e4f946ba76a53124ee50.js
+++ precache-manifest.d4c83398b8e6e4f946ba76a53124ee50.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e43f4eda810effa96131ca4992533139",
+    "revision": "c0212735625a4d1d50d4a7b54d83e192",
     "url": "/react-portfolio/index.html"
   },
   {
-    "revision": "e898aedb25737d8f7139",
+    "revision": "c829a71b8efc69ed29ea",
     "url": "/react-portfolio/static/css/main.16ddee95.chunk.css"
   },
   {
-    "revision": "aa497b71d6a89abddf4c",
-    "url": "/react-portfolio/static/js/2.08867fc8.chunk.js"
+    "revision": "67489cc172239691389b",
+    "url": "/react-portfolio/static/js/2.3ccb52d7.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/react-portfolio/static/js/2.08867fc8.chunk.js.LICENSE.txt"
+    "url": "/react-portfolio/static/js/2.3ccb52d7.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e898aedb25737d8f7139",
-    "url": "/react-portfolio/static/js/main.3077fd27.chunk.js"
+    "revision": "c829a71b8efc69ed29ea",
+    "url": "/react-portfolio/static/js/main.7f3cb91c.chunk.js"
   },
   {
     "revision": "aff33cef9dbb8787d6b3",
